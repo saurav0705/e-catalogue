2f824bec9907d352f662ec931e64af432f43e177
Saurav Aggarwal <saurav.aggarwal@flipkart.com>
2020-10-19 11:25:17 +0530

    altered webpack

--- src/store/thunk/imageCarasoul.js
+++ src/store/thunk/imageCarasoul.js
@@ -1,5 +1,6 @@
-import config from '../../config.json'
+import config from 'config'
 import axios from 'axios'
+import { CARASOUL } from '../paths.js'
 import {
   fetchImagesOfCarasoul,
   successImagesOfCarasoul,
@@ -8,7 +9,7 @@ import {
 
 export const getCarasoulImages = () => (dispatch) => {
   dispatch(fetchImagesOfCarasoul())
-  return axios.get(config.API_LOCAL + 'images').then(
+  return axios.get(config.API + CARASOUL).then(
     (resp) => dispatch(successImagesOfCarasoul(resp)),
     (err) => dispatch(errorImagesOfCarasoul(err)),
   )
